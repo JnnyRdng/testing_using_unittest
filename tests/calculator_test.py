fa25c0d2222b1cec91c59ce5a1aef48384f9760c
Jonny Reading <jonnyreading@gmail.com>
2020-07-21 18:26:30 +0100

    Third test asserts divide function

--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -4,11 +4,18 @@ from src.calculator import add, divide, multiply, subtract
 class TestCalculator(unittest.TestCase):
     
     def test_add(self):
-        # test that the add() method in calculator produces the correct results
+        # test that the add() function in calculator produces the correct results
         self.assertEqual(7, add(2, 5))
 
     def test_subtract(self):
-        # test that the subtract() method performs as expected
+        # test that the subtract() function performs as expected
         expected = 4
         actual = subtract(10, 6)
         self.assertEqual(expected, actual)
+
+    def test_divide(self):
+        # test for divide function
+        expected = 3
+        actual = divide(21, 7)
+        self.assertEqual(expected, actual)
+

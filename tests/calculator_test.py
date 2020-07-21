3f84daf0a1354b6a571f55b45e94a18e4762b1da
Jonny Reading <jonnyreading@gmail.com>
2020-07-21 18:19:26 +0100

    Tests the calculator subtract function

--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -6,3 +6,9 @@ class TestCalculator(unittest.TestCase):
     def test_add(self):
         # test that the add() method in calculator produces the correct results
         self.assertEqual(7, add(2, 5))
+
+    def test_subtract(self):
+        # test that the subtract() method performs as expected
+        expected = 4
+        actual = subtract(10, 6)
+        self.assertEqual(expected, actual)

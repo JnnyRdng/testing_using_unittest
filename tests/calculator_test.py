b507b8fa103197446210ca9328bcd64f2355ad91
Jonny Reading <jonnyreading@gmail.com>
2020-07-21 18:13:00 +0100

    test_add() function to verify 2 + 5 = 7

--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -2,4 +2,7 @@ import unittest
 from src.calculator import add, divide, multiply, subtract
 
 class TestCalculator(unittest.TestCase):
-    pass
+    
+    def test_add(self):
+        # test that the add() method in calculator produces the correct results
+        self.assertEqual(7, add(2, 5))
